d1decbd4b7cf3fda20d1ac5c5ea4a2cd5f85fc3d
crucialfelix <crucialfelix@gmail.com>
2016-03-22 18:22:14 +0100

    add Miso.Dataset and parse CSV files

--- app/actions/datasets.js
+++ app/actions/datasets.js
@@ -1,10 +1,17 @@
 import {SELECT_DATASET, SET_DATASETS, OPEN_DATASET_DIALOG} from '../actionTypes';
 import callActionOnMain from '../ipc/callActionOnMain';
 
+let Miso = require('miso.dataset');
+let jetpack = require('fs-jetpack');
+let extname = require('path').extname;
+let basename = require('path').basename;
+
 export function setDataset(path, data, metadata) {
+  let name = basename(path, extname(path));
   return {
     type: SELECT_DATASET,
     payload: {
+      name: name,
       path,
       data,
       metadata
@@ -20,11 +27,34 @@ export function openDatasetDialog() {
   };
 }
 
+let parsers = {
+  '.json': Miso.Dataset.Parsers.Obj,  // already parsed to data
+  '.csv': Miso.Dataset.Parsers.Delimited
+};
+
 export function loadDataset(path) {
   return (dispatch) => {
-    // load file or use external tool to analyze it
-    // read metadata
-    // dispatch(setDataset(path, data, metadata));
+    let ext = extname(path);
+    let parser = parsers[ext];
+
+    if (!parser) {
+      // dispatch error action
+      console.error('Filetype not supported:', ext);
+      return;
+    }
+
+    let readAs = ext === '.json' ? 'json' : 'utf8';
+    jetpack.readAsync(path, readAs).then((data) => {
+
+      let ds = new Miso.Dataset({
+        data: data,
+        parser: parser
+      });
+
+      ds.fetch().then((data) => {
+        dispatch(setDataset(path, data));
+      }, console.error);
+    }, console.error);
   };
 }
 
