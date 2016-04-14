00bb86d527ca87bc5896cbd94a53a888b0a8cb24
crucialfelix <crucialfelix@gmail.com>
2016-04-14 16:27:08 +0200

    cleanup

--- app/utils/mapping.js
+++ app/utils/mapping.js
@@ -1,6 +1,13 @@
 import d3 from 'd3';
 import * as _ from 'lodash';
 
+/**
+ * Detect type and extent of values
+ * and create a function that scales values to range.
+ *
+ * This uses d3's scale functions which do not have the musical functions
+ * of supercollider.
+ */
 export function autoScale(values, range) {
 
   // empty set
@@ -8,21 +15,26 @@ export function autoScale(values, range) {
     return d3.scale.linear().domain([0, 1]).range(range);
   }
 
-  let first = values[0];
+  const first = values[0];
 
   // time series
   if (_.isDate(first)) {
-    let extent = d3.extent(values);
-    return d3.time.scale().domain(extent).nice().range([range[0], range[1] * 0.95]);
+    return d3.time.scale()
+      .domain(d3.extent(values)).nice()
+      .range([range[0], range[1] * 0.95]);
   }
 
   // ordinals, class and category strings
   if (_.isString(first)) {
-    let domain = Array.from((new Set(values)));
+    const domain = Array.from((new Set(values)));
     // if domain is length 1 then its not useful for mapping
-    return d3.scale.ordinal().domain(domain).rangeRoundPoints(range);
+    return d3.scale.ordinal()
+      .domain(domain)
+      .rangeRoundPoints(range);
   }
 
   // normal linear
-  return d3.scale.linear().domain(d3.extent(values)).nice().range(range);
+  return d3.scale.linear()
+    .domain(d3.extent(values)).nice()
+    .range(range);
 }
