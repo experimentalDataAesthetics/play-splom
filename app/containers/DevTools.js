3090d2d20ec1f759c4b3d43d1fee15ba86add3e3
crucialfelix <crucialfelix@gmail.com>
2016-06-26 15:14:10 +0200

    revert to simple redux LogMonitor

--- app/containers/DevTools.js
+++ app/containers/DevTools.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createDevTools } from 'redux-devtools';
-// import LogMonitor from 'redux-devtools-log-monitor';
+import LogMonitor from 'redux-devtools-log-monitor';
 import DockMonitor from 'redux-devtools-dock-monitor';
-import FilterableLogMonitor from 'redux-devtools-filterable-log-monitor';
+// import FilterableLogMonitor from 'redux-devtools-filterable-log-monitor';
 
 export default createDevTools(
   <DockMonitor
@@ -10,6 +10,6 @@ export default createDevTools(
     changePositionKey="ctrl-q"
     defaultIsVisible={false}
   >
-    <FilterableLogMonitor />
+    <LogMonitor />
   </DockMonitor>
 );
