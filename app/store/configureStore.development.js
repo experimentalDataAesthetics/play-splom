dcaf917075d95bf68a0e15d7c9eea77468f4867b
Chris Sattinger <crucialfelix@gmail.com>
2017-05-21 16:57:12 +0200

    fix: hotloading of redux reducers

--- app/store/configureStore.development.js
+++ app/store/configureStore.development.js
@@ -20,7 +20,9 @@ export default function configureStore(initialState) {
   const store = createStore(rootReducer, initialState, enhancer);
 
   if (module.hot) {
-    module.hot.accept('../reducers', () => store.replaceReducer(require('../reducers')));
+    module.hot.accept('../reducers', () =>
+      store.replaceReducer(require('../reducers/index').default)
+    );
   }
 
   return store;
